8e5a4e1216effe5b30cfd6eb77d49de0c6ecdc4b
koussay-r <koussayr98@gmail.com>
2023-03-06 17:27:13 +0100

    getting started

---
+++ back-end/server.js
@@ -0,0 +1,90 @@
+const Pusher = require("pusher");
+const express=require("express")
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+const cors=require("cors")
+const taskmodel=require("./model/taskModel.js")
+//server config
+const app=express()
+dotenv.config()
+const port=process.env.PORT||9000
+//midlewares
+app.use(express.json())
+app.use(cors())
+mongoose.set('strictQuery', true)
+//db congfig
+const mongourl=`mongodb+srv://admin:${process.env.PASSWORD}@cluster0.yde1grw.mongodb.net/todoapp?retryWrites=true&w=majority`
+mongoose.connect(mongourl,{
+    useNewUrlParser:true
+})
+//endpoints
+app.post("/create",async(req,res)=>{
+    try{
+        const ress=await taskmodel.create(req.body)
+        res.status(201).send(ress)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.get("/",async(req,res)=>{
+    try{
+        
+        const ress=await taskmodel.find()
+        res.status(200).send(ress)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.delete("/:id",async(req,res)=>{
+    try{
+        const ress=await taskmodel.findByIdAndDelete({_id:req.params.id})
+        res.status(202).send(ress)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "eu",
+  useTLS: true
+});
+const db=mongoose.connection;
+db.once("open",()=>{
+    console.log("db connected")
+    const collection=db.collection("tasks")
+    const changeStream=collection.watch()
+    changeStream.on("change",(change)=>{
+        if(change.operationType=="insert"){
+            const task=change.fullDocument;
+            pusher.trigger("my-channel", "my-event", {
+                _id:task._id,
+              value: task.value,
+              __v:task.__v
+            });
+            
+        }
+        else if(change.operationType=="delete"){
+            pusher.trigger("my-channel", "my-event", {
+                _id:"",
+              value:"",
+              __v:""
+            });
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+//listening
+app.listen(port,()=>{
+    console.log(`server starting on port : ${port}`)
+})
---
+++ front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/main'
+function App() {
+  return (
+    <div className="">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
